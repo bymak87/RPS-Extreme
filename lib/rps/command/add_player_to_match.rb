f914c4f76a9862a156c549894f9b9cad432ab215
Amy Bakameyer <bymak87@gmail.com>
2014-05-20 12:00:52 -0500

    add_player_to_match_method

--- lib/rps/command/add_player_to_match.rb
+++ lib/rps/command/add_player_to_match.rb
@@ -7,9 +7,7 @@ module RPS
       p2_id = input[:p2_id]
       match_id = input[:match_id]
 
-      # player1 = RPS.db.get_user(id)
-      # player2 = RPS.db.get_user(id)
-      # match = RPS.db.get_match(match_id)
+
 
       add_player = RPS.db.add_player_to_match(p1_id, p2_id, match_id)
       success(add_player: add_player, match: match)
